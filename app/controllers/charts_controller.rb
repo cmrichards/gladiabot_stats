0f948554d04c084ddc4a7f5d14c7b47602e903e5
Chris Richards <chrisr@salltd.co.uk>
2017-04-16 00:09:26 +0100

    Added player history elo chart to global charts

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -9,6 +9,7 @@ class ChartsController < ApplicationController
       if @form.opponent.blank?
         elo_range_successes = EloRangeSuccess.create_elo_ranges(@form)
         @elo_range_chart = EloRangeSuccessChart.new(@form, elo_range_successes)
+  
       end
 
       elo_dates = EloDate.create_elo_dates(@form)
@@ -31,6 +32,8 @@ class ChartsController < ApplicationController
   # GET /charts/global
 
   def global
+    player_elos = PlayerElo.create_player_elos
+    @player_elo_chart = PlayerEloChart.new(player_elos)
   end
 
   private
@@ -95,6 +98,93 @@ class ChartsController < ApplicationController
         end
       end
       message
+    end
+  end
+
+  class PlayerEloChart
+
+    def initialize(player_elos)
+      @player_elos = player_elos.sort_by { |pe|
+        pe.elos.last.elo_delta
+      }.reverse
+    end
+
+    def title
+      "Player Elos"
+    end
+
+    def y_axis_title
+      "Elo"
+    end
+
+    def categories
+      @player_elos.map(&:player).map(&:name)
+    end
+
+    def series
+        @player_elos.map do |player_elo|
+          [ player_elo.player.name,  player_elo.elos ]
+        end
+    end
+  end
+
+  class PlayerElo
+
+    attr_accessor :player, :elos
+
+    class Elo
+      attr_accessor :date,
+                    :position,
+                    :elo_delta
+    end
+
+    def self.create_player_elos
+      top_player_ids = Game.find_by_sql("
+        select 
+        game_players.player_id
+        from games
+        inner join game_players on game_players.game_id = games.id
+        where
+        resolution_time > now() - interval '7 days'
+        group by game_players.player_id
+        order by max(game_players.current_elo) desc
+        limit 20").map(&:player_id)
+
+      sql =[
+        "SELECT
+          game_players.player_id,
+          players.name,
+          games.resolution_time,
+          game_players.current_elo,
+          game_players.elo_delta
+        FROM
+          games
+          inner join game_players on game_players.game_id = games.id
+          left outer join players on players.id = game_players.player_id
+        WHERE
+          games.resolution_time > :start_time
+          and game_players.player_id in (:player_ids)
+        ",
+        {
+          start_time: Time.now - 3.weeks,
+          player_ids: top_player_ids
+        }
+      ]
+      Game.find_by_sql(sql).
+        group_by(&:player_id).map do |player_id, player_rows|
+        player_rows = player_rows.sort_by(&:resolution_time)
+        elo = player_rows.first.current_elo
+        PlayerElo.new.tap do |pe|
+          pe.player = Player.new(id: player_id, name: player_rows.first.name)
+          pe.elos = player_rows.group_by{ |pr| pr.resolution_time.to_date }.map { |date, p_rows|
+            elo += p_rows.sum(&:elo_delta)
+            PlayerElo::Elo.new.tap do |pe| 
+              pe.date = date
+              pe.elo_delta = elo
+            end
+          }
+        end
+      end.sort_by{ |pe| pe.elos.last.elo_delta }
     end
   end
 
